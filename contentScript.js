9227a68d79f39f29a898780cc258a1d6be5b464b
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2024-12-02 16:20:35 +0530

    All Done

--- contentScript.js
+++ contentScript.js
@@ -1,32 +1,4 @@
 (() => {
-    function muteYoutubeAdsAndSkip() {
-        const video = document.querySelector('video');
-        const cnt = document.querySelector('#movie_player');
-        const muteButton = document.querySelector('.ytp-mute-button');
-        const isAdPlaying = cnt.classList.contains('ad-showing');
-        let skipAdBtn = document.querySelector(".ytp-skip-ad-button");
-        if (!video) return;
-        // Mute logic
-        if (isAdPlaying && !video.muted) {
-            //automaticvally clicks mute button 
-            muteButton.click();
-        } else if (!isAdPlaying && video.muted) {
-            //automaticvally clicks mute button 
-            muteButton.click();
-        }
-
-        //skipping logic
-        if (isAdPlaying) {
-            if (skipAdBtn.style.display === '') {
-                simulateEnterKey(skipAdBtn);
-                console.log('Ad skipped logic');
-            }
-        }
-    }
-    setInterval(muteYoutubeAdsAndSkip, 500);
-
-
-
     // Function to simulate a mouse click
     function simulateEnterKey(element) {
         if (element) {
@@ -41,6 +13,36 @@
             console.log('Dispatch event being called');
         }
     }
+
+    function muteYoutubeAdsAndSkip() {
+        const video = document.querySelector('video');
+        const cnt = document.querySelector('#movie_player');
+        const muteButton = document.querySelector('.ytp-mute-button');
+        const isAdPlaying = cnt.classList.contains('ad-showing');
+        let skipAdBtn = document.querySelector(".ytp-skip-ad-button");
+        let previewAdBtn = document.querySelector(".ytp-preview-ad");
+        if (!video) return;
+        // Mute logic
+        if (isAdPlaying && !video.muted) {
+            //automaticvally clicks mute button 
+            muteButton.click();
+        } else if (!isAdPlaying && video.muted) {
+            //automaticvally clicks mute button 
+            muteButton.click();
+        }
+
+        //skipping logic
+        if (isAdPlaying) {
+            //skip button appears immediately
+            skipAdBtn.style = '';
+            previewAdBtn.style.display = 'none'
+            // simulateEnterKey(skipAdBtn); //not working
+            skipAdBtn.focus();
+            console.log('Ad skipped logic');
+        }
+    }
+    setInterval(muteYoutubeAdsAndSkip, 100);
+    
 })();
 
 
